b30578d9ec28f761c111315c25c98a5ce7ef7f28
Gabriel Moraes <gabrielhm2001@gmail.com>
2023-08-12 00:52:54 -0300

    add health route

--- src/app.ts
+++ src/app.ts
@@ -7,3 +7,8 @@ export const app = fastify()
 
 app.register(cookie)
 app.register(transactionsRoutes, { prefix: '/transactions' })
+
+// eslint-disable-next-line require-await -- Fastify requires this to be async
+app.get('/health', async (_req, reply) => {
+  reply.send({ status: 'ok' })
+})
